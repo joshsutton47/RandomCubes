a1aa1bf8c6bbff59266e59ae17037f03b186741d
joshsutton47 <56270887+joshsutton47@users.noreply.github.com>
2022-01-24 16:35:13 -0600

    Unity Script and Prefab (1.24)
    Created the RandomCubes script and a cube prefab for it to spawn.

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,28 @@
+/***
+ *  Created by: Josh Sutton
+ *  Date created: 24 January 2022
+ *  
+ *  Last edited by: Josh Sutton
+ *  Last edited date: 24 January 2022
+ *  
+ *  Description: spawns multiple cube prefabs into scene.
+ * 
+ ***/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
